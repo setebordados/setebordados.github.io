cb6626d0c88462754a89e7cad2d3ade3126b48d6
setebordados <setebordados@gmail.com>
2026-07-12 21:19:34 -0300

    Altera loja do Elo7 para a Shopee no site

--- copiar_fotos.bat
+++ copiar_fotos.bat
@@ -3,7 +3,7 @@ echo Preparando para copiar as imagens...
 mkdir assets 2>nul
 copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\8d56ee61-d3b2-42f8-826c-46ac533d7879\media__1777086270051.png" "assets\logo.png" >nul
 copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\8d56ee61-d3b2-42f8-826c-46ac533d7879\media__1777087668010.png" "assets\matriz.png" >nul
-copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\af2592d1-7823-4676-9149-f74d1d5712ac\media__1777677798168.png" "assets\elo7.png" >nul
+copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\8a51921d-14b4-44d0-a047-f8c1c081f3c9\media__1783901616939.png" "assets\shopee.png" >nul
 copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\af2592d1-7823-4676-9149-f74d1d5712ac\media__1777678162874.png" "assets\uniforme.png" >nul
 copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\af2592d1-7823-4676-9149-f74d1d5712ac\media__1777678543180.png" "assets\diversos.png" >nul
 copy "C:\Users\Sete Bordados\.gemini\antigravity\brain\af2592d1-7823-4676-9149-f74d1d5712ac\media__1777678753236.png" "assets\polos_pretas.png" >nul
